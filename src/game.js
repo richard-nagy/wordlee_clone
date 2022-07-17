f582336a39cbc27605353002eb55edd8efa9b9df
Richárd Nagy <nagy.richard@thisisdone.com>
2022-07-17 14:31:26 +0200

    Replace Bootstrap with MUI

--- src/game.js
+++ src/game.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
-import { Col, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import Button from "@mui/material/Button";
+import { Grid, styled } from "@mui/material";
+
+const StyledTd = styled("td")(() => ({
+    color: "white",
+    border: "2px solid white",
+    width: "50px",
+    height: "50px",
+    textAlign: "center",
+    fontSize: "24px",
+    fontWeight: "bold",
+}));
 
 // prettier-ignore
-const abc = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S",
-"D", "F", "G", "H", "J", "K", "L", "Enter", "Z", "X", "C", "V", "B", "N", "M", "<-"];
+const abc = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F",
+"G", "H", "J", "K", "L", "Enter", "Z", "X", "C", "V", "B", "N", "M", "<-"];
 
 export default function Game() {
     const [input, setInput] = useState("");
+    const [guesses, setGuesses] = useState([]);
 
     function readPressedKey(key) {
         key = key.toUpperCase();
@@ -18,8 +28,10 @@ export default function Game() {
             return;
         }
 
-        if (key === "ENTER" && input.length == 5) {
-            alert("5 char long :)");
+        if (key === "ENTER" && input.length === 5) {
+            // alert("5 char long :)");
+            setGuesses([...guesses, input]);
+            setInput("");
             return;
         }
 
@@ -33,36 +45,67 @@ export default function Game() {
             onKeyDown={(e) => readPressedKey(e.key)}
             tabIndex="0"
             className="bg-dark"
-            style={{ height: "500px", width: "600px" }}
+            style={{ height: "700px", width: "700px" }}
         >
-            <Row xs={1} md={5} className="g-1" style={{ width: "275px" }}>
-                {Array.from({ length: 30 }).map((_, i) => (
-                    <Col>
-                        <Card
-                            style={{ width: "50px", height: "50px" }}
-                            className="border-primary bg-dark text-light"
-                        >
-                            {input.charAt(i, i + 1)}
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+            <style type="text/css">
+                {`
+                .btn- {
+                    background-color: purple;
+                    color: white;
+                }
+                `}
+            </style>
 
-            <h3 style={{ color: "white", height: "40px" }}>{input}</h3>
+            <table style={{ borderSpacing: "10px", borderCollapse: "separate" }}>
+                <tbody>
+                    {guesses.map((e) => {
+                        return (
+                            <tr>
+                                {e.split("").map((element) => {
+                                    return <StyledTd>{element}</StyledTd>;
+                                })}
+                            </tr>
+                        );
+                    })}
+                    <tr>
+                        {Array.from({ length: 5 }).map((_, i) => {
+                            return guesses.length < 5 ? (
+                                <StyledTd>{input.charAt(i)}</StyledTd>
+                            ) : null;
+                        })}
+                    </tr>
+                    {Array.from({ length: 5 - guesses.length }).map((_, i) => (
+                        <tr>
+                            <StyledTd></StyledTd>
+                            <StyledTd></StyledTd>
+                            <StyledTd></StyledTd>
+                            <StyledTd></StyledTd>
+                            <StyledTd></StyledTd>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
             <br />
             <div style={{ width: "370px" }}>
-                {abc.map((e) => {
-                    return (
-                        <Button
-                            variant="secondary"
-                            className="m-1 p-2"
-                            onClick={() => readPressedKey(e)}
-                        >
-                            {e}
-                        </Button>
-                    );
-                })}
+                <Grid container columnSpacing={10} rowSpacing={1}>
+                    {abc.map((e) => {
+                        return (
+                            <Grid item xs={1}>
+                                <Button
+                                    sx={{ color: "black", backgroundColor: "lightGray" }}
+                                    variant="contained"
+                                    onClick={() => readPressedKey(e)}
+                                >
+                                    {e}
+                                </Button>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
             </div>
+            {guesses.map((e) => {
+                return e;
+            })}
         </div>
     );
 }
